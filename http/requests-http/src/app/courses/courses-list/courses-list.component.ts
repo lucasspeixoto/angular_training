439c954ce6e531da7dfccfae915f2627cd90210a
Lucas Peixoto <lspeixoto@stefanini.com>
2021-05-23 09:41:27 -0300

    Configuração HttpClient e configuração da listagem de cursos na tabela

--- http/requests-http/src/app/courses/courses-list/courses-list.component.ts
+++ http/requests-http/src/app/courses/courses-list/courses-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './courses';
+
+import { CoursesService } from './../courses.service';
 
 @Component({
   selector: 'app-courses-list',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesListComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[]
+
+  constructor(
+    private service: CoursesService
+  ) { }
 
   ngOnInit(): void {
+    this.service.list().subscribe(
+      data => this.courses = data
+    )
+
   }
 
 }
---
+++ http/requests-http/src/app/courses/courses.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators'
+
+import { Course } from './courses-list/courses';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = 'http://localhost:3000/courses'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  list() {
+    return this.http.get<Course[]>(this.API)
+    .pipe(
+      tap(console.log)
+    )
+  }
+
+}
