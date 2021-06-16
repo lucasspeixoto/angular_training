8be23b509a59e24388549e61983e5626bd58b0ef
Lucas Peixoto <lspeixoto@stefanini.com>
2021-06-16 08:51:01 -0300

    Funcionalidade de pesquisa das libs com base no endpoint da api cdnjs

--- http/requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ http/requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -8,15 +11,46 @@ import { FormControl } from '@angular/forms';
 })
 export class LibSearchComponent implements OnInit {
 
-  queryField = new FormControl()
+  queryField = new FormControl();
 
-  constructor() { }
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
 
-  ngOnInit(): void {
+  results$: Observable<any>;
+
+  total: number;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+
   }
 
   onSearch() {
-    console.log(this.queryField.value)
+
+    const fields = 'name,description,version,homepage'
+
+    let inputSearch = this.queryField.value
+    if (inputSearch && (inputSearch = inputSearch.trim()) !== '') {
+
+      const params_ = {
+        search: inputSearch,
+        fields: fields
+      }
+
+      //Outra forma de criar um objeto de parâmetros
+      let params = new HttpParams()
+      params = params.set('search', inputSearch)
+      params = params.set('fields', fields) 
+
+      this.results$ = this.http.get(this.SEARCH_URL, { params })
+        .pipe(
+          tap((res: any) => this.total = res.total),
+          map((res: any) => res.results)
+        )
+    }
+
   }
 
 }
