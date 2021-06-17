db54ced3c35b85b4b264725cbd4cf19e2b5758b4
Lucas Peixoto <lspeixoto@stefanini.com>
2021-06-16 22:06:11 -0300

    Inclusão da busca reativa

--- http/requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ http/requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -19,12 +20,30 @@ export class LibSearchComponent implements OnInit {
 
   total: number;
 
+  readonly FIELDS: string = 'name,description,version,homepage'
+
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
-
+    //valueChanges retorna um observable
+    this.results$ = this.queryField.valueChanges  
+      .pipe(
+        map(value => value.trim()),
+        filter(value => value.length > 1),
+        debounceTime(200), //200 ms de espera antes de disparar a próxima requisição (para não disparar ao digitar todas as letras)
+        distinctUntilChanged(), //Se repetir o valor não manda a requisição, só quando alterar
+        tap(value => console.log(value)),
+        switchMap(value => this.http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })), //Transforma o valor que recebemos em outro observable (Sempre cancela requisições anteriores)
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+        )     
   }
 
   onSearch() {
